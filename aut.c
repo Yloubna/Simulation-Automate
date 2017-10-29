2a68d04f5899cc57e1aedd608f4416e13f50fb4c
Loubna Yousfi <loubnaysf.ly@gmail.com>
2017-10-29 18:56:50 +0100

    Update aut.c

--- aut.c
+++ aut.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 // #include <stdbool.h>
 
 int main() {
 
 // Déclarations :
-
 
 int w[4]={1,0,0}; // la chaine d'entrée
 int i;
@@ -19,10 +19,41 @@ int state=0;
 int next_state;
 int res = 8; // 8 vaut False
 
+/* --- read w from file ----- */
 
+char *p; // to split
+char *array[3];
 
+char newC[5]; // to convert
+char c;
+
+FILE *file;
+file = fopen("test.txt", "r");
+if (file) {
+    while ((c = getc(file)) != EOF){
+    // convert int c  into  string newC
+    snprintf(newC ,5, "%c", c);
+}
+  fclose(file);
+}
+// Split string into tokens
+
+for (i = 0; i < 3; i++)
+{
+    p = strtok (&c, "/");
+    array[i] = p;
+
+}
+
+// display tokens
+
+for (i = 0; i < 3; i++){
+    printf("%s\n", array[i]);
+}
+
+/* ----------------------------------- */
 // Dialogue :
-printf(" you entered : \n");
+printf(" you entered : ");
 for ( i = 0; i < 3; i++) {
   printf("%d ", w[i]);
 }
@@ -40,6 +71,5 @@ if (state == Final) {
 }
 
 printf("\n (9 is True, 8 is False) your word is  %d \n",res);
-
 
 }
